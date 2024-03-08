526ab445a9cbf354b8b5065a3aaabc3f614a7efd
Диана <diana7ponomareva@yandex.ru>
2024-03-08 20:11:35 +0300

    Task1

--- OOP1/src/main/java/org/example/Task1/Task1.java
+++ OOP1/src/main/java/org/example/Task1/Task1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.lang.Math;
 import java.util.UUID;
 public class Task1{
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите количество телефонов");
