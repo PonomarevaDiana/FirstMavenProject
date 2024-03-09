3f7e0e20b52f3993221ad139f08a91069d6af9f7
Диана <diana7ponomareva@yandex.ru>
2024-03-09 16:46:27 +0300

    Task5

---
+++ OOP.part1/src/main/java/org/example/Task5/PizzaOrder.java
@@ -0,0 +1,95 @@
+package org.example.Task5;
+public class PizzaOrder {
+    private String title;
+
+    public enum Size {
+        SMALL(false),
+        MEDIUM(false),
+        BIG(false);
+        public boolean size;
+
+        Size(boolean size) {
+            this.size = size;
+        }
+
+        public void setMEDIUM(boolean size) {
+            this.size = size;
+        }
+
+        public void setBIG(boolean size) {
+            this.size = size;
+        }
+
+        public void setSMALL(boolean size) {
+            this.size = size;
+        }
+
+    }
+
+    private boolean isSauce;
+    Size size;
+    private String address;
+    private boolean isOrderAccepted = false;
+
+    public PizzaOrder(String title, Size size, String address) {
+        this.title = title;
+        this.size = size;
+        this.address = address;
+    }
+
+    public PizzaOrder() {
+
+    }
+
+    public void order() {
+        if (isOrderAccepted == true) System.out.println("Заказ уже принят");
+        else {
+            isOrderAccepted = true;
+            System.out.println("Заказ принят. ");
+            if (size == size.BIG) System.out.print("Большая ");
+            else if (size == size.MEDIUM) System.out.print("Средняя ");
+            else System.out.print("Маленькая ");
+            System.out.print("пицца " + title);
+            if (isSauce == true) System.out.print(" с соусом");
+            else System.out.print(" без соуса");
+            System.out.println(" на адрес " + address + ". ");
+        }
+    }
+
+    public void cancel() {
+        if (isOrderAccepted == false) System.out.println("Заказ отменен или заказ не был принят");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public boolean getIsSauce() {
+        return isSauce;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setIsSauce(boolean isSauce) {
+        this.isSauce = isSauce;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    //метод отмены заказа toString1
+    public void toString1() {
+        isOrderAccepted = false;
+    }
+}
---
+++ OOP.part1/src/main/java/org/example/Task5/Task5.java
@@ -0,0 +1,34 @@
+package org.example.Task5;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PizzaOrder pizza = new PizzaOrder();
+        System.out.println("Введите название пиццы");
+        pizza.setTitle(in.nextLine());
+        System.out.println("Введите размер пиццы: большая, средняя или маленькая");
+        String s = in.nextLine();
+        if (s == "большая") pizza.size.BIG.setBIG(true);
+        else if (s == "средняя") pizza.size.MEDIUM.setBIG(true);
+        else if (s == "маленькая") pizza.size.SMALL.setBIG(true);
+
+        System.out.println("Нужен ли соус? Напишите true или false");
+        pizza.setIsSauce(in.nextBoolean());
+        in.nextLine();
+        System.out.println("По какому адресу вы хотите забрать пиццу?");
+        pizza.setAddress(in.nextLine());
+        //метод cancel(заказ еще не сделан)
+        pizza.cancel();
+        //метод order(заказываем)
+        pizza.order();
+        //метод cnacel(заказ сделан)
+        pizza.cancel();
+        //попытка второй раз заказать
+        pizza.order();
+        //отмечняем заказ
+        pizza.toString1();
+        pizza.cancel();
+    }
+}
